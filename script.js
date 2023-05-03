3eb2b0b818b283fce4f2c157b0a3b7a5470afd2c
deepupradeep47 <pradeepraghavendra47@gmail.com>
2023-05-03 21:07:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//console.log("hellll")
+//window.alert("hey huu")
+//let user=window.prompt("namee!")
+//window.alert("hey "+user+"huu")
+let a=10
+let b="dkmskj"
+let c="A"
+let d=null // object type
+console.log(typeof a)
+
